96d190620782ac4590ce13e8739b1dc66ecf5321
Federico <fluque87@gmail.com>
2024-09-27 13:18:28 +0200

    Uso de route y activatedRoute

--- src/app/personas/formulario/formulario.component.ts
+++ src/app/personas/formulario/formulario.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Persona } from '../../persona.model';
 import { LoggingService } from '../../LoggingService.service';
 import { PersonasService } from '../../personas.service';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-formulario',
@@ -10,23 +10,38 @@ import { Router } from '@angular/router';
   styleUrl: './formulario.component.css',
 })
 export class FormularioComponent {
-  nombreInput: string = '';
-  apellidoInput: string = '';
+  nombreInput: string;
+  apellidoInput: string;
+  index: number;
 
   /* Usamos un constructor para inyectar los servicios */
   constructor(
     private loggingService: LoggingService,
     private personasService: PersonasService,
-    private router:Router
+    private router:Router,
+    private route: ActivatedRoute
   ) {
     this.personasService.saludar.subscribe((indice: number) =>
       alert('El indice es: ' + indice)
     );
   }
 
+  ngOnInit(){
+    this.index = this.route.snapshot.params["id"];
+    if(this.index){
+      let persona: Persona = this.personasService.encontrarPersona(this.index);
+      this.nombreInput = persona.nombre;
+      this.apellidoInput = persona.apellido;
+    }
+  }
+
   guardarPersona() {
     let persona1 = new Persona(this.nombreInput, this.apellidoInput);
-    this.personasService.agregarPersona(persona1);
-    this.router.navigate(["personas"]);
+    if(this.index){
+      this.personasService.modificarPersona(this.index, persona1);
+    }else{
+      this.personasService.agregarPersona(persona1);
+    }
+      this.router.navigate(["personas"]); 
   }
 }
